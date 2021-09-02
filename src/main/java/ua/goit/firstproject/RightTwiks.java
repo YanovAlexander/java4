7740ab8619366b921e170e2f6b8e5f4079692d63
Oleksandr Yanov1 <oleksandr_yanov1@epam.com>
2021-09-02 20:50:03 +0300

    first commit

---
+++ src/main/java/ua/goit/firstproject/RightTwiks.java
@@ -0,0 +1,6 @@
+package ua.goit.firstproject;
+
+public class RightTwiks {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/ua/goit/second/LeftTwiks.java
@@ -0,0 +1,11 @@
+package ua.goit.second;
+
+
+import ua.goit.firstproject.RightTwiks;
+
+public class LeftTwiks {
+    public static void main(String[] args) {
+        RightTwiks  rt = new RightTwiks();
+        RightTwiks.main(new String[]{"Hello"});
+    }
+}
